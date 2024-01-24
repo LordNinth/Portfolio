c9b2aa84729ff1eb69139777600433073b0b4eb7
LordNinth <rr621801@gmail.com>
2024-01-24 09:06:46 +0000

    prototype

---
+++ app4Prototype.js
@@ -0,0 +1,5 @@
+//Prototype is a specific object/template that stores every method and properties for every array/string.
+//Idea is object contains common properties and methods, that copies of other array/strings look up to find their common properties. Its a blueprint/template.
+
+// Array.prototype is the actual object where we add methods/ properties . This is template object/prototype
+// __proto__: Is a property name on any given Array/string. __proto__ is the property name that is used to reference prototype/ actual object
