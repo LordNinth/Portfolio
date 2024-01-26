26a39c7f6d1f997e3ecde5c99ae45c8f909a1279
LordNinth <rr621801@gmail.com>
2024-01-26 02:14:29 +0000

    node js

---
+++ app6Node.js
@@ -0,0 +1,14 @@
+//1: Node is implementation of Js outside of browser
+//Usecase
+//i) It is used for creating web server. writing server side logic for FS applications
+//without node, we can write code in the browser that can send request to server. We can send request to API with AJAX and do something with browser.
+// But what if I want to build my own APi and I want to work with DB. Build full application. Node enables us to overcome this.
+
+//ii) Build native applications such as Vs studio / slack 
+
+//iii) Build games e.g crossCode
+
+// node repl: read evaluate print and loop
+//use to debug, play around with new tool/feature we install. We can also write JS. Think of it like: Node equivalent of JS console in chrome
+//in node. Global is equivalent to Browser Window. It contains all the functions. Inside node, it is the top level bubble
+//In node, we dont have any of the browser functions DOM API. But we have other built in modules which help us interact with files and OS.
