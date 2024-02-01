1de6fb950739ef3da7a5ebd93e068424e0befede
LordNinth <rr621801@gmail.com>
2024-02-01 06:57:49 +0000

    revision

--- Node/math.js
+++ Node/math.js
@@ -6,6 +6,7 @@ const square = (x) => x * x;
 // 1) module.exports.add = add //by default it is empty obj. This is what I want to explicitly export
 // module.exports.pi = pi
 // module.exports.square = square or exports.square = square
+//module.export by default gives us empty object
 
 // 2) build on object and export that object
 
@@ -16,11 +17,3 @@ const math = {
 };
 
 module.exports.math = math;
-
-
-// TLDR: We can require file we create, we dont automatically get acesss to all files. We have to explicitly set module.export to include whatever properties /func variable we want to export. 
-//Modular export by default is empty obj. We can add properties to that. We can change modular export variable to anything but then it wont be exported. So dont use export as a variable name
-
-//3: npm init is easy way to make json, package.json contains meta data abt projects like dependencies and so on
-//It is useful for other team members to know what dependencies they need to use
-// As a collaborator, I can download the code, go to package.json and type "npm install" to get all dependencies
---
+++ Node/node.md
@@ -0,0 +1,16 @@
+1: File exporting on node:
+In client side, we can include src in .html page. However, server side we need to export it.
+syntax: module.exports.add = add
+
+We can require file we create, we don't automatically get access to all files. 
+We have to explicitly set module.export to include whatever properties /func variable we want to export. 
+Modular export by default is empty obj and we can add properties to that. 
+We can change modular export variable to anything but then it wont be exported. So don't use export as a variable name
+
+2: npm init is easy way to make json, package.json contains meta data abt projects like dependencies and so on. 
+It is useful for other team members to know what dependencies they need to use
+As a collaborator, I can download the code, go to directory with package.json and type "npm install" to get all dependencies. It saves us lot of time
+
+To download modules globally. npm i -g [module name]. All packages are usually downloaded locally.
+
+3: npm i figlet to resolve error "not found package.json" in node module.
