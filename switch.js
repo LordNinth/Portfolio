3f56af4b93d54b285f6884baf178dc4e97919341
LordNinth <rr621801@gmail.com>
2024-05-16 21:14:51 +0100

    switch: if/while

---
+++ switch.js
@@ -0,0 +1,65 @@
+function sayHello(country, time) {
+    let greeting;
+    let isMorning = time > 0 && time <12;
+    let isEvening = time > 11 && time <24;
+    let invalidTime = !isMorning && !isEvening; 
+    
+    while(isMorning === true){
+    if(country === "Spain"){
+        greeting ="buenos dias"
+        console.log(greeting);
+        break;
+    }
+    else if (country === "Mexico")
+    {
+        greeting ="buenos dias"
+        console.log(greeting);
+        break;
+    } 
+    else if (country ==="France")
+    {
+        greeting ="bon matin"
+        console.log(greeting);
+        break;
+    }
+    else {
+        greeting = null
+        console.log(greeting);
+        break;
+        }}
+    
+    while(isEvening){
+        if(country === "Spain"){
+            greeting = "buenas noches"
+            console.log(greeting);
+            break
+        }
+        else if (country === "Mexico")
+        {
+        greeting ="buenas noches"
+        console.log(greeting);
+        break;
+        } 
+        else if (country ==="France")
+        {
+            greeting ="bon soir"
+            console.log(greeting);
+            break;
+        }
+        else {
+            greeting = null
+            console.log(greeting);
+            break;
+        }}
+
+    while(invalidTime){
+        greeting = null
+        console.log(greeting)
+        break
+    
+        }
+    // Don't change code below this line
+    return greeting;
+}
+
+
